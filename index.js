a7db6fcf704c6370639286c9b181c9dd9f146cee
aquinogonzalo <aquinoalejandro666@gmail.com>
2023-05-19 11:16:37 -0300

    Se logró unir los recursos para que el hexadecimal tenga nombre

--- index.js
+++ index.js
@@ -1,21 +1,30 @@
+// Obtener el elemento con el ID "start-button" y agregar un event listener para el evento "click"
 document.getElementById("start-button").addEventListener("click", () => {
+    // Obtener el elemento con el ID "result"
     const resultElement = document.getElementById("result");
 
+    // Verificar si la propiedad "EyeDropper" está disponible en el objeto global "window"
     if (!window.EyeDropper) {
-        resultElement.textContent =
-        "Tu navegador no soporta esta extension, prueba con navegadores basados en Chromium";
+        // Si no está disponible, mostrar un mensaje de error en el elemento "result"
+        resultElement.textContent = "Tu navegador no soporta esta extensión, prueba con navegadores basados en Chromium";
         return;
     }
 
+    // Crear una instancia de EyeDropper
     const eyeDropper = new EyeDropper();
+
+    // Abrir la herramienta EyeDropper y esperar a que el usuario seleccione un color
     eyeDropper
         .open()
         .then((result) => {
-        resultElement.textContent = result.sRGBHex;
-        resultElement.style.backgroundColor = result.sRGBHex;
+            // Cuando se selecciona un color, actualizar el contenido del elemento "result" con el valor del color en formato sRGB hexadecimal
+            resultElement.textContent = result.sRGBHex;
+
+            // Establecer el color de fondo del elemento "result" con el valor del color en formato sRGB hexadecimal
+            resultElement.style.backgroundColor = result.sRGBHex;
         })
         .catch((e) => {
-        resultElement.textContent = e;
+            // Si ocurre un error durante el proceso de selección de color, mostrar el error en el elemento "result"
+            resultElement.textContent = e;
         });
-
 });
