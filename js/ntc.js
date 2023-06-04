961d78aa3b345ed19e6c43596c38ec39a698d480
aquinogonzalo <aquinoalejandro666@gmail.com>
2023-06-04 03:03:45 -0300

    Implementado el hexa, se separo el codigo del eyedropper del ntc, arreglado todos los problemas, a la espera de traduccion total de colores

--- js/ntc.js
+++ js/ntc.js
@@ -1,8 +1,3 @@
-/*---------------------------+---------------------------------------+---------------------*/
-/*--------------------------/ FUSION DE API DE EYE DROPPER Y NTC JS /---------------------*/
-/*-------------------------+---------------------------------------+---------------------*/
-
-
 /*
 ---------------------------------------------------------------------------------------------------------------
 |-----------------------------------------------------------------|
@@ -11,25 +6,7 @@
 
 All the functions, code, lists etc. have been written specifically
 for the Name that Color JavaScript by Chirag Mehta unless otherwise
-specified.
-
-This script is released under the: Creative Commons License:
-Attribution 2.5 http://creativecommons.org/licenses/by/2.5/
-
-Sample Usage:
-
-  <script type="text/javascript" src="ntc.js"></script>
-
-  <script type="text/javascript">
-
-    let n_match  = ntc.name("#6195ED");
-    n_rgb        = n_match[0]; // This is the RGB value of the closest matching color
-    n_name       = n_match[1]; // This is the text string for the name of the match
-    n_exactmatch = n_match[2]; // True if exact color match, False if close-match
-
-    alert(n_match);
-
-  </script>
+specified.Complete colloquial spanish translation by EquipoCodex for DearColors project, one nav extension for Color Blind.
 
 */
 
@@ -1707,55 +1684,5 @@ let ntc = {
 ]
 
 }
-
-
-// Obtener el elemento con el ID "start-button" y agregar un event listener para el evento "click"
-document.getElementById("start-button").addEventListener("click", () => {
-  // Obtener el elemento con el ID "result"
-  const resultElement = document.getElementById("result");
-
-  // Verificar si la propiedad "EyeDropper" está disponible en el objeto global "window"
-  if (!window.EyeDropper) {
-      // Si no está disponible, mostrar un mensaje de error en el elemento "result"
-      resultElement.textContent = "Tu navegador no soporta esta extensión, prueba con navegadores basados en Chromium";
-      return;
-  }
-
-  // Crear una instancia de EyeDropper
-  const eyeDropper = new EyeDropper();
-
-  // Abrir la herramienta EyeDropper y esperar a que el usuario seleccione un color
-  eyeDropper
-      .open()
-      .then((result) => {
-          // Obtener el nombre del color seleccionado y ser mostrado en pantalla
-          const n_match  = ntc.name(result.sRGBHex);
-          const n_rgb        = n_match[0]; // This is the RGB value of the closest matching color
-          const n_name       = n_match[1]; // This is the text string for the name of the match
-          const n_exactmatch = n_match[2]; // True if exact color match, False if close-match
-          
-          document.getElementById('result').innerText = n_name
-
-
-          const hexa = document.getElementById('hexa')
-          
-          if (hexa.checked){
-            document.getElementById('result-hexa').innerText = n_rgb
-          }
-          if (hexa.checked){
-            hexa.addEventListener("change", () => {
-              document.getElementById('result-hexa').innerText = ""
-            })
-          }
-
-      })
-      .catch((e) => {
-          // Si ocurre un error durante el proceso de selección de color, vuelve a ser el mensaje del inicio
-          resultElement.textContent = "...";
-      });
-      
-});
-
-
 
 ntc.init();
