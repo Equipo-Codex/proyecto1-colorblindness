9ebedae0f3eed58a66043c73765326c42ebb158f
aquinogonzalo <aquinoalejandro666@gmail.com>
2023-06-09 12:49:55 -0300

    e

--- js/eyedropper.js
+++ js/eyedropper.js
@@ -11,53 +11,56 @@ document.getElementById("start-button").addEventListener("click", () => {
         // Si no está disponible, mostrar un mensaje de error en el elemento "result"
         resultElement.textContent = "Tu navegador no soporta esta extensión, prueba con navegadores basados en Chromium";
         return;
-    }
+    }            
+        // Crear una instancia de EyeDropper
+        const eyeDropper = new EyeDropper();
 
-    // Crear una instancia de EyeDropper
-    const eyeDropper = new EyeDropper();
-
-    // Función para mostrar u ocultar la página
-    const togglePageVisibility = () => {
-        if (isPageVisible) {
-            document.body.style.display = "none"; // Ocultar la página
-        } else {
-            document.body.style.display = ""; // Mostrar la página
-        }
-        isPageVisible = !isPageVisible; // Invertir el estado de visibilidad
-    };
-
-    // Abrir la herramienta EyeDropper y esperar a que el usuario seleccione un color
-    eyeDropper
-        .open()
-        .then((result) => {
-            // Obtener el nombre del color seleccionado y mostrarlo en pantalla
-            const n_match = ntc.name(result.sRGBHex);
-            const n_rgb = n_match[0]; // This is the RGB value of the closest matching color
-            const n_name = n_match[1]; // This is the text string for the name of the match
-
-            document.getElementById('result').innerText = n_name;
-
-            const hexa = document.getElementById('hexa');
-
-            if (hexa.checked) {
-                document.getElementById('result-hexa').innerText = n_rgb;
-
-                hexa.addEventListener("change", () => {
-                    document.getElementById('result-hexa').innerText = "";
-                });
+        // Función para mostrar u ocultar la página
+        const togglePageVisibility = () => {
+            if (isPageVisible) {
+                document.body.style.display = "none"; // Ocultar la página
+            } else {
+                document.body.style.display = ""; // Mostrar la página
             }
+            isPageVisible = !isPageVisible; // Invertir el estado de visibilidad
+        };
+        //Inicia
+        setTimeout(() => {
+        // Abrir la herramienta EyeDropper y esperar a que el usuario seleccione un color
+        eyeDropper
+            .open()
+            .then((result) => {
+                // Obtener el nombre del color seleccionado y mostrarlo en pantalla
+                const n_match = ntc.name(result.sRGBHex);
+                const n_rgb = n_match[0]; // This is the RGB value of the closest matching color
+                const n_name = n_match[1]; // This is the text string for the name of the match
 
-            // Mostrar u ocultar la página al finalizar la selección de color
-            togglePageVisibility();
-        })
-        .catch((e) => {
-            // Si ocurre un error durante el proceso de selección de color, vuelve a ser el mensaje del inicio
-            resultElement.textContent = "...";
+                document.getElementById('result').innerText = n_name;
 
-            // Mostrar u ocultar la página en caso de error
-            togglePageVisibility();
-        });
+                const hexa = document.getElementById('hexa');
 
+                if (hexa.checked) {
+                    document.getElementById('result-hexa').innerText = n_rgb;
+
+                    hexa.addEventListener("change", () => {
+                        document.getElementById('result-hexa').innerText = "";
+                    });
+                }
+
+                // Mostrar u ocultar la página al finalizar la selección de color
+                togglePageVisibility();
+            })
+            .catch((e) => {
+                // Si ocurre un error durante el proceso de selección de color, vuelve a ser el mensaje del inicio
+                resultElement.textContent = "...";
+
+                // Mostrar u ocultar la página en caso de error
+                togglePageVisibility();
+            });
+        }, 150);
+        
     // Mostrar u ocultar la página al hacer clic en el botón
-    togglePageVisibility();
+        togglePageVisibility();
+    //Hace la captura despues de que se oculte
+        activateAndCapture();
 });
